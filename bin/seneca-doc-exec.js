4890ba8121e126ca37f390259830c2e60c953af0
rjrodger <richard@ricebridge.com>
2019-02-08 19:16:05 +0000

    inspect

--- bin/seneca-doc-exec.js
+++ bin/seneca-doc-exec.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
-console.log('hi there')
+const Inspect = require('../lib/inspect.js')
+
+const LocalPackage = require('./package.json')
+const LocalFolder = process.cmd()
+
+console.log(LocalFolder, LocalPackage)
+
+const spec = inspect(LocalFolder, LocalPackage)
+console.log(spec)
+
+
