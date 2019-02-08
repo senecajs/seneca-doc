74348f3b7908260fe326045208ca56c1115e6996
rjrodger <richard@ricebridge.com>
2019-02-08 18:16:04 +0000

    first

---
+++ seneca-doc.js
@@ -0,0 +1,6 @@
+
+module.exports = seneca_doc
+
+function seneca_doc(options) {
+
+}
---
+++ test/seneca-dec.test.js
@@ -0,0 +1,28 @@
+/* Copyright (c) 2019 voxgig and other contributors, MIT License */
+'use strict'
+
+const Util = require('util')
+
+const Lab = require('lab')
+const Code = require('code')
+const lab = (exports.lab = Lab.script())
+const expect = Code.expect
+
+const PluginValidator = require('seneca-plugin-validator')
+const Seneca = require('seneca')
+const Plugin = require('..')
+
+
+lab.test('validate', Util.promisify(function(x,fin){PluginValidator(Plugin, module)(fin)}))
+
+lab.test('happy', async () => {
+  return await seneca_instance().ready()
+})
+
+
+function seneca_instance(config,plugin_options) {
+  return Seneca(config,{legacy:{transport:false}})
+    .test()
+    .use('promisify')
+    .use(Plugin, plugin_options)
+}
