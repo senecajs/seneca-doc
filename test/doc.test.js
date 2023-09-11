59e3724dc6d7f802de40dae9fc5fd19588a61ac1
Richard Rodger <richard@ricebridge.com>
2023-09-11 14:51:58 +0100

    jest

--- test/doc.test.js
+++ test/doc.test.js
@@ -1,133 +1,128 @@
-/* Copyright (c) 2019 voxgig and other contributors, MIT License */
+/* Copyright (c) 2019-2023 voxgig and other contributors, MIT License */
 'use strict'
 
 const Fs = require('fs')
 
-const Lab = require('@hapi/lab')
-const Code = require('@hapi/code')
-const lab = (exports.lab = Lab.script())
-const expect = Code.expect
-
-const PluginValidator = require('seneca-plugin-validator')
 const Seneca = require('seneca')
 const Plugin = require('..')
 const Render = require('../lib/render')
 const Inject = require('../lib/inject')
 
-lab.test('validate', PluginValidator(Plugin, module))
 
-lab.test('happy', async () => {
-  return await seneca_instance().ready()
-})
 
-lab.test('action-validation', async () => {
-  var si = await seneca_instance()
-    .quiet()
-    .use('joi')
-    .use('./p0')
-    .ready()
+describe('doc', ()=>{
+  test('happy', async () => {
+    return await makeSeneca().ready()
+  })
 
-  // Load from <name>-doc.js
+  test('action-validation', async () => {
+    var si = await makeSeneca()
+        .quiet()
+        .use('joi')
+        .use('./p0')
+        .ready()
 
-  var a1o1 = await si.post('p:p0,a:1,x:x')
-  expect(a1o1.y).equal('x')
+    // Load from <name>-doc.js
 
-  try {
-    await si.post('p:p0,a:1', { x: 1 })
-    Code.fail()
-  } catch (e) {
-    expect(e.message).contains('"x" must be a string')
-  }
+    var a1o1 = await si.post('p:p0,a:1,x:x')
+    expect(a1o1.y).toEqual('x')
 
-  var a2o1 = await si.post('p:p0,a:2,x:x')
-  expect(a2o1.y).equal('x')
+    try {
+      await si.post('p:p0,a:1', { x: 1 })
+      Code.fail()
+    } catch (e) {
+      expect(e.message).toContain('"x" must be a string')
+    }
 
-  var a2o2 = await si.post('p:p0,a:2', { x: 1 })
-  expect(a2o2.y).equal(1)
+    var a2o1 = await si.post('p:p0,a:2,x:x')
+    expect(a2o1.y).toEqual('x')
 
-  // Load from `doc` prop in plugin meta return
+    var a2o2 = await si.post('p:p0,a:2', { x: 1 })
+    expect(a2o2.y).toEqual(1)
 
-  await si.use('./p1').ready()
-  // TODO: allow doc extraction from export
+    // Load from `doc` prop in plugin meta return
 
-  a1o1 = await si.post('p:p1,a:1,x:x')
-  expect(a1o1.y).equal('x')
+    await si.use('./p1').ready()
+    // TODO: allow doc extraction from export
 
-  try {
-    await si.post('p:p1,a:1', { x: 1 })
-    Code.fail('did not validate')
-  } catch (e) {
-    expect(e.message).contains('"x" must be a string')
-  }
+    a1o1 = await si.post('p:p1,a:1,x:x')
+    expect(a1o1.y).toEqual('x')
 
-  a2o1 = await si.post('p:p1,a:2,x:x')
-  expect(a2o1.y).equal('x')
+    try {
+      await si.post('p:p1,a:1', { x: 1 })
+      Code.fail('did not validate')
+    } catch (e) {
+      expect(e.message).toContain('"x" must be a string')
+    }
 
-  a2o2 = await si.post('p:p1,a:2', { x: 1 })
-  expect(a2o2.y).equal(1)
+    a2o1 = await si.post('p:p1,a:2,x:x')
+    expect(a2o1.y).toEqual('x')
 
-  // Load from `doc` prop in plugin object
+    a2o2 = await si.post('p:p1,a:2', { x: 1 })
+    expect(a2o2.y).toEqual(1)
 
-  await si.use('./p2').ready()
-  // TODO: allow doc extraction from export
+    // Load from `doc` prop in plugin object
 
-  a1o1 = await si.post('p:p2,a:1,x:x')
-  expect(a1o1.y).equal('x')
+    await si.use('./p2').ready()
+    // TODO: allow doc extraction from export
 
-  try {
-    await si.post('p:p2,a:1', { x: 1 })
-    Code.fail('did not validate')
-  } catch (e) {
-    expect(e.message).contains('"x" must be a string')
-  }
+    a1o1 = await si.post('p:p2,a:1,x:x')
+    expect(a1o1.y).toEqual('x')
 
-  a2o1 = await si.post('p:p2,a:2,x:x')
-  expect(a2o1.y).equal('x')
+    try {
+      await si.post('p:p2,a:1', { x: 1 })
+      Code.fail('did not validate')
+    } catch (e) {
+      expect(e.message).toContain('"x" must be a string')
+    }
 
-  a2o2 = await si.post('p:p2,a:2', { x: 1 })
-  expect(a2o2.y).equal(1)
-})
+    a2o1 = await si.post('p:p2,a:2,x:x')
+    expect(a2o1.y).toEqual('x')
 
-lab.test('describe-plugin', async () => {
-  var si = await seneca_instance().ready()
-  var out = await si.post('sys:doc,describe:plugin', { plugin: 'doc' })
-  expect(out.plugin).equal('doc')
-  expect(out.actions[0].pattern).equal('describe:plugin,sys:doc')
-})
+    a2o2 = await si.post('p:p2,a:2', { x: 1 })
+    expect(a2o2.y).toEqual(1)
+  })
 
-lab.test('describe-pin', async () => {
-  var si = await seneca_instance().ready()
-  var out = await si.post('sys:doc,describe:pin', { pin: 'role:seneca' })
-  expect(out.actions.length).above(0)
-})
+  test('describe-plugin', async () => {
+    var si = await makeSeneca().ready()
+    var out = await si.post('sys:doc,describe:plugin', { plugin: 'doc' })
+    expect(out.plugin).toEqual('doc')
+    expect(out.actions[0].pattern).toEqual('describe:plugin,sys:doc')
+  })
 
-lab.test('options_section', async () => {
-  var si = await seneca_instance().ready()
-  var out = await si.post('sys:doc,describe:plugin', { plugin: 'doc' })
-  var md = Render.options(out)
-  expect(md[0].text).contains('## Options')
+  test('describe-pin', async () => {
+    var si = await makeSeneca().ready()
+    var out = await si.post('sys:doc,describe:pin', { pin: 'role:seneca' })
+    expect(out.actions.length>0).toEqual(true)
+  })
 
-  // TODO: convert to using Gubu
-  // expect(md).contains('`test`')
-})
+  test('options_section', async () => {
+    var si = await makeSeneca().ready()
+    var out = await si.post('sys:doc,describe:plugin', { plugin: 'doc' })
+    var md = Render.options(out)
+    expect(md[0].text).toContain('## Options')
 
-lab.test('action_list', async () => {
-  var si = await seneca_instance().ready()
-  var out = await si.post('sys:doc,describe:plugin', { plugin: 'doc' })
-  var md = Render.action_list(out)[0].text
-  expect(md).contains('"describe":"plugin","sys":"doc"')
-  expect(md).contains('"describe":"pin","sys":"doc"')
-})
+    // TODO: convert to using Gubu
+    // expect(md).toContain('`test`')
+  })
 
-lab.test('action_desc', async () => {
-  var si = await seneca_instance().ready()
-  var out = await si.post('sys:doc,describe:plugin', { plugin: 'doc' })
-  var md = Render.action_desc(out)[0].text
-  expect(md).contains('"describe":"plugin","sys":"doc"')
-})
+  test('action_list', async () => {
+    var si = await makeSeneca().ready()
+    var out = await si.post('sys:doc,describe:plugin', { plugin: 'doc' })
+    var md = Render.action_list(out)[0].text
+    expect(md).toContain('"describe":"plugin","sys":"doc"')
+    expect(md).toContain('"describe":"pin","sys":"doc"')
+  })
 
-lab.test('update_source', async () => {
-  const src = `
+  test('action_desc', async () => {
+    var si = await makeSeneca().ready()
+    var out = await si.post('sys:doc,describe:plugin', { plugin: 'doc' })
+    var md = Render.action_desc(out)[0].text
+    expect(md).toContain('"describe":"plugin","sys":"doc"')
+  })
+
+  test('update_source', async () => {
+    const src = `
 a
 <!--START:foo-->
 b
@@ -138,12 +133,12 @@ d
 <!--END:bar-->
 e
 `
-  var out = Inject.update_source(src, {
-    foo: [{ name: 'foo', text: 'BBB' }],
-    bar: [{ name: 'bar', text: 'DDD' }]
-  })
+    var out = Inject.update_source(src, {
+      foo: [{ name: 'foo', text: 'BBB' }],
+      bar: [{ name: 'bar', text: 'DDD' }]
+    })
 
-  expect(out).equal(`
+    expect(out).toEqual(`
 a
 <!--START:foo-->
 BBB
@@ -154,38 +149,40 @@ DDD
 <!--END:bar-->
 e
 `)
-})
-
-lab.test('update_file', async () => {
-  var foo_text = '' + Math.random()
-  var bar_text = '' + Math.random()
-
-  Inject.update_file(__dirname + '/test.md', {
-    foo: [{ name: 'foo', text: '' + foo_text }],
-    bar: [{ name: 'bar', text: '' + bar_text }]
   })
 
-  var out = Fs.readFileSync(__dirname + '/test.md').toString()
-  expect(out.indexOf(foo_text)).above(-1)
-  expect(out.indexOf(bar_text)).above(-1)
+  test('update_file', async () => {
+    var foo_text = '' + Math.random()
+    var bar_text = '' + Math.random()
+
+    Inject.update_file(__dirname + '/test.md', {
+      foo: [{ name: 'foo', text: '' + foo_text }],
+      bar: [{ name: 'bar', text: '' + bar_text }]
+    })
+
+    var out = Fs.readFileSync(__dirname + '/test.md').toString()
+    expect(out.indexOf(foo_text)>-1).toEqual(true)
+    expect(out.indexOf(bar_text)>-1).toEqual(true)
+  })
+
+  test('render-intern-nicepat', async () => {
+    var rin = Render.intern.nicepat
+    var top = ['sys', 'role']
+    expect(rin('a:1,sys:foo', top)).toEqual('"sys":"foo","a":1')
+    expect(rin('b:2,sys:foo,a:1', top)).toEqual('"sys":"foo","a":1,"b":2')
+    expect(rin('a:1,role:bar', top)).toEqual('"role":"bar","a":1')
+    expect(rin('b:2,role:bar,a:1', top)).toEqual('"role":"bar","a":1,"b":2')
+    expect(rin('sys:foo,a:1,role:bar', top)).toEqual(
+      '"role":"bar","sys":"foo","a":1'
+    )
+    expect(rin('b:2,role:bar,a:1,sys:foo', top)).toEqual(
+      '"role":"bar","sys":"foo","a":1,"b":2'
+    )
+  })
+
 })
 
-lab.test('render-intern-nicepat', async () => {
-  var rin = Render.intern.nicepat
-  var top = ['sys', 'role']
-  expect(rin('a:1,sys:foo', top)).equal('"sys":"foo","a":1')
-  expect(rin('b:2,sys:foo,a:1', top)).equal('"sys":"foo","a":1,"b":2')
-  expect(rin('a:1,role:bar', top)).equal('"role":"bar","a":1')
-  expect(rin('b:2,role:bar,a:1', top)).equal('"role":"bar","a":1,"b":2')
-  expect(rin('sys:foo,a:1,role:bar', top)).equal(
-    '"role":"bar","sys":"foo","a":1'
-  )
-  expect(rin('b:2,role:bar,a:1,sys:foo', top)).equal(
-    '"role":"bar","sys":"foo","a":1,"b":2'
-  )
-})
-
-function seneca_instance(config, plugin_options) {
+function makeSeneca(config, plugin_options) {
   return Seneca(config, { legacy: { transport: false } })
     .test()
     .use('promisify')
